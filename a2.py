de89f94e651740c860344d23eb8871d4732e06b5
prateeksha-coder <prateeksha.codingal@gmail.com>
2025-09-20 20:36:21 +0530

    commit

--- a2.py
+++ a2.py
@@ -1,6 +1,6 @@
 n = int(input("enter number of rows"))
 
-for i in range(0, n):#outer loop for rows
+for i in range(0, n):#outer loop for rows      
 
 	for j in range(0, i+1):#inner loop for columns
 		print("*", end=" ")
